fe1a582559ae0f7f906a313df277f136d780554e
Hoadv <68044489+vanhoa690@users.noreply.github.com>
2024-03-26 15:05:00 +0700

    done login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,14 +1,14 @@
 import User from "../models/UserModel";
-import bcrypt from "bcrypt";
-import { registerValidate } from "../validations/auth";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { registerValidator, loginValidator } from "../validations/auth";
 
 class AuthController {
-  // POST: auth/register
   async register(req, res) {
     try {
       //B1: validate: email, password, username
       const { email, username, avatar, password } = req.body;
-      const { error } = registerValidate.validate(req.body);
+      const { error } = registerValidator.validate(req.body);
       if (error) {
         const errors = error.details.map((err) => err.message);
         return res.status(400).json({
@@ -21,7 +21,7 @@ class AuthController {
         return res.status(400).json({ message: "Email da duoc dang ky" });
       }
       // b3 ma hoa password
-      const hashPassword = await bcrypt.hash(password, 10);
+      const hashPassword = await bcryptjs.hash(password, 10);
       // update db
       const user = await User.create({
         email,
@@ -41,9 +41,41 @@ class AuthController {
     }
   }
 
-  // POST: auth/login
-  login(req, res) {
-    res.send("Login");
+  // POST: auth/login: email, password
+  async login(req, res) {
+    const { email, password } = req.body;
+    //B1: validate: email, password
+    const { error } = loginValidator.validate(req.body);
+    if (error) {
+      const errors = error.details.map((err) => err.message);
+      return res.status(400).json({
+        message: errors,
+      });
+    }
+    // check email xem co trong db
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      return res.status(404).json({
+        message: "Tai khoan ko hop he",
+      });
+    }
+    // so sanh password: bcryptjs
+    const checkPassword = await bcryptjs.compare(password, checkUser.password);
+    if (!checkPassword) {
+      return res.status(404).json({
+        message: "Tai khoan ko hop he",
+      });
+    }
+    // ma hoa token
+    const token = jwt.sign({ id: checkUser._id }, "khoa-bi-mat", {
+      expiresIn: "1d",
+    });
+    // res
+    res.status(200).json({
+      message: "Login ok",
+      user: { ...checkUser.toObject(), password: undefined },
+      token,
+    });
   }
 }
 
--- src/validations/auth.js
+++ src/validations/auth.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-const registerValidate = Joi.object({
+const registerValidator = Joi.object({
   username: Joi.string().min(3).max(10).required().messages({
     "any.required": "Username thieu roi",
     "string.min": "Username phai nhieu hon 3 ky tu",
@@ -15,4 +15,11 @@ const registerValidate = Joi.object({
   abortEarly: false,
 });
 
-export { registerValidate };
+const loginValidator = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+}).options({
+  abortEarly: false,
+});
+
+export { registerValidator, loginValidator };
