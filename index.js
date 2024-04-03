2e5452816d7c556bbb262822f5fca02ccc016b34
Hoadv <68044489+vanhoa690@users.noreply.github.com>
2024-04-03 15:07:36 +0700

    6

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
 app.use(express.json());
 
 const port = process.env.PORT || 8000;
-const dbUrl = process.env.DB_URI || "mongodb://127.0.0.1:27017/db_movies";
+// const dbUrl = process.env.DB_URI || "mongodb://127.0.0.1:27017/db_movies";
 // connectMongoDB(dbUrl);
 
 app.use("/", router);
