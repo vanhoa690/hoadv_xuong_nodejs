ff9fb5d6deea77474cc71f2b9edf378c3d273e24
Hoadv <68044489+vanhoa690@users.noreply.github.com>
2024-04-03 14:26:43 +0700

    fix deploy

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import router from "./routes";
-import connectMongoDB from "./config/dbconfig";
-import errorHandler from "./middlewares/errorHandler";
+import router from "./routes/index.js";
+import connectMongoDB from "./config/dbconfig.js";
+import errorHandler from "./middlewares/errorHandler.js";
 dotenv.config();
 const app = express();
 app.use(
@@ -12,11 +12,12 @@ app.use(
 );
 app.use(express.json());
 
-const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/db_movies";
+const port = process.env.PORT || 8000;
+const dbUrl = process.env.DB_URI || "mongodb://127.0.0.1:27017/db_movies";
 connectMongoDB(dbUrl);
 
 app.use("/", router);
 
 app.use(errorHandler);
 
-export const viteNodeApp = app;
+app.listen(port, () => console.log("Server running " + port));
