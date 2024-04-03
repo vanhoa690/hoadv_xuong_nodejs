e4d9e555439369e2bd083042b82dc0c1296d9568
Hoadv <68044489+vanhoa690@users.noreply.github.com>
2024-04-03 14:46:12 +0700

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import router from "./src/routes/index.js";
+import connectMongoDB from "./src/config/dbconfig.js";
+import errorHandler from "./src/middlewares/errorHandler.js";
+dotenv.config();
+const app = express();
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+const dbUrl = process.env.DB_URI || "mongodb://127.0.0.1:27017/db_movies";
+connectMongoDB(dbUrl);
+
+app.use("/", router);
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log("Server is running with " + port));
