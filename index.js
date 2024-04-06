40614ab27d099e1dd0b45f19bd657001b1c7570d
Hoadv <68044489+vanhoa690@users.noreply.github.com>
2024-04-06 15:20:12 +0700

    done api docs

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
+import YAML from 'yaml'
 import router from "./src/routes/index.js";
 import connectMongoDB from "./src/config/dbconfig.js";
 import errorHandler from "./src/middlewares/errorHandler.js";
 dotenv.config();
+
+const file  = fs.readFileSync('./api-docs.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
 const app = express();
 app.use(
   express.urlencoded({
@@ -20,5 +27,6 @@ connectMongoDB(dbUrl);
 app.use("/", router);
 
 app.use(errorHandler);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log("Server is running "));
